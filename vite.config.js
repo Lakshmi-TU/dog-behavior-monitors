064c4c9b1f064914f9dbc48788aa167d9aee5bc8
Lakshmi-TU <lakshmitu5@gmail.com>
2026-05-01 20:18:42 +0530

    Fixed vite config merge conflict

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dog-behavior-monitors/'  // 👈 VERY IMPORTANT
-<<<<<<< HEAD
+  base: '/dog-behavior-monitors/'  // 👈 correct for GitHub Pages
 })
-=======
-})
->>>>>>> c0987791ef2f635838740e75b62a8688e21deb92
